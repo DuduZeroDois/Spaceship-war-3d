44b831588bf6b82ba72c4fae2611011ffe45b005
Eduardo Sampaio <ebbsamp@gmail.com>
2025-09-10 17:01:10 -0300

    atualização no html e continuação não terminada no js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+//Vars
+const pontos = document.getElementById("ponto")
+const lf = document.getElementById("vida")
+const gameOver = document.getElementById("perdeu")
+const Win = document.getElementById("ganhou")
+const restart = document.getElementsByClassName("Reset")
+let pontosL = 0
+let lfL = 10
+let GameOver = false
+
+
 //criar cena
 const Cena = new THREE.Scene()
 Cena.background = new THREE.Color(0x000000)
@@ -25,9 +36,65 @@ LuizD.position.set(5, 5, 5)
 Cena.add(LuizD)
 Camera.position.z = 5
 
+//criar balas
+let balas = []
+function CriarBala() {
+    if (gameOver) {
+        return
+    }
+    const BalaGEO = new THREE.BoxGeometry(0.1, 0.3, 0.1)// w, h, profundida
+    const BalaMat = new THREE.MeshStandartMaterial({color:0xFFE900})
+    const Bala = new THREE.Mesh(BalaGEO, BalaMat)
+    Bala.position.set(Nave.position.x, Nave.position.y + 0.5, Nave.position.z)
+    Cena.add(Bala)
+    balas.push(Bala)
+}
+
+//teclas
+const teclas = {}
+document.addEventListener("keydown", (e)=> {
+    teclas[e.key] = true
+    if (e.key === " ") {
+        CriarBala()
+    }
+})
+document.addEventListener("keyup", (e)=> teclas[e.key] = false)
+
+//checar se deu GameOver
+function checkGameOver() {
+    for (let inimigo of inimgos) {
+      if (  inimigo.position.y <= Nave.position.y + 0.5 && inimigo.position.x == Nave.position.x){
+        return true
+    }
+    return lfL <= 0
+}}
+
 //animação
 function Anima() {
+    if (GameOver) {
+        return
+    }
     requestAnimationFrame(Anima)
+    if (teclas["a"] || teclas["ArrowLeft"]) {
+        Nave.position.x -= 0.1        
+    }
+        if (teclas["d"] || teclas["ArrowRight"]) {
+        Nave.position.x += 0.1        
+    }
+    balas.forEach((bala, index)=> {
+        bala.position.y += 0.3
+        if (bala.position.y > 5) {
+            Cena.remove(bala)
+            balas.splice(index, 1)
+        }
+    })
+    //bala colide? soon
+    if (checkGameOver()) {
+        GameOver = true
+        gameOver.style.display = "flex"
+        return
+    }
+
     Render.render(Cena, Camera)
 }
 Anima()
