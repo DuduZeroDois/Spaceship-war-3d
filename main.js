ed2c7e05c3b5cc94fd72497daf5b67f748b462fe
Eduardo Sampaio <ebbsamp@gmail.com>
2025-09-17 16:54:02 -0300

    continuação não terminada do js

--- main.js
+++ main.js
@@ -28,13 +28,7 @@ const Nave = new THREE.Mesh(NaveGEO, NaveMat)
 Nave.position.y = -3
 Cena.add(Nave)
 
-//luz
-const Luiz = new THREE.AmbientLight(0xffffff, 0.5)
-Cena.add(Luiz)
-const LuizD = new THREE.DirectionalLight(0xffffff, 1)
-LuizD.position.set(5, 5, 5)
-Cena.add(LuizD)
-Camera.position.z = 5
+
 
 //criar balas
 let balas = []
@@ -50,6 +44,30 @@ function CriarBala() {
     balas.push(Bala)
 }
 
+//criar inimigos
+let inimigos = []
+function CriarI() {
+    inimigos = []
+    for (let i = -3; i <= 3; i += 1.5) {
+        const InimigoGEO = new THREE.BoxGeometry(0.8, 0.5, 0.5)
+        const InimigoMAT = new THREE.MeshStandardMaterial({color:0xFF0000})
+        const Inimigo = new THREE.Mesh(InimigoGEO, InimigoMAT)
+        Inimigo.position.set(i, 2.5, 0)
+        Inimigo.userData.velocity = 0.01 + Math.random() * 0.01
+        Cena.add(Inimigo)
+        inimigos.push(Inimigo)
+    }
+}
+CriarI()
+
+//luz
+const Luiz = new THREE.AmbientLight(0xffffff, 0.5)
+Cena.add(Luiz)
+const LuizD = new THREE.DirectionalLight(0xffffff, 1)
+LuizD.position.set(5, 5, 5)
+Cena.add(LuizD)
+Camera.position.z = 5
+
 //teclas
 const teclas = {}
 document.addEventListener("keydown", (e)=> {
@@ -62,7 +80,7 @@ document.addEventListener("keyup", (e)=> teclas[e.key] = false)
 
 //checar se deu GameOver
 function checkGameOver() {
-    for (let inimigo of inimgos) {
+    for (let inimigo of inimigos) {
       if (  inimigo.position.y <= Nave.position.y + 0.5 && inimigo.position.x == Nave.position.x){
         return true
     }
@@ -88,12 +106,42 @@ function Anima() {
             balas.splice(index, 1)
         }
     })
-    //bala colide? soon
+    //bala colide?
+    balas.forEach((bala, bindex)=> {
+        inimigos.forEach((inimigo, index)=> {
+        if (Math.abs(bala.position.x - inimigo.position.x) < 0.5 && Math.abs(bala.position.y - inimigo.position.y) < 0.5) {
+            Cena.remove(bala)
+            balas.splice(bindex, 1)
+            explodir (inimigo)
+            inimigos.splice(index, 1)
+            if (inimigos.length === 0 && !GameOver) {
+                GameOver = true
+                Win.style.display = "flex"
+                return
+            }
+            pontosL += 1
+            pontos.textContent = pontosL
+        }
+        })
+    })
+
+    //checkGameOver
     if (checkGameOver()) {
         GameOver = true
         gameOver.style.display = "flex"
         return
     }
+
+    //Ganhar Movimento I
+    inimigos.forEach((inimigo)=> {
+        inimigo.position.y -= inimigo.userData.velocity
+        if (inimigo.position.y < Nave.position.y - 0.5) {
+            lfL -= 1
+            lf.textContent = lfL
+            inimigo.position.y = 3 + Math.random()
+            inimigo.position.x = -3 + Math.random() *6
+        }
+    })
 
     Render.render(Cena, Camera)
 }
